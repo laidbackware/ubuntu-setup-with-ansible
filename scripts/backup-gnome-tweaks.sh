cfffd799f0b933bcb1ab76f1e039b5a966a8dc8a
Matt Proud <laidbackware.git@gmail.com>
2023-02-16 14:25:26 +0000

    add chrome hack add gnome settings add gnome extensions

--- scripts/backup-gnome-tweaks.sh
+++ scripts/backup-gnome-tweaks.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+set -eu
 # Utility to backup current Gnome tweaks
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-dconf dump / > ${script_dir}/../roles/gnome/tweaks/files/saved_settings.dconf
+
+function dump() {
+  name="$1"
+  path="$2"
+  role="$3"
+
+  echo "dumping $name config"
+  dconf dump "$path" > "${script_dir}/../roles/gnome/${role}/files/${name}.conf"
+}
+
+dump "desktop" "/org/gnome/desktop/interface/" "theme"
+dump "user-theme" "/org/gnome/shell/extensions/user-theme/" "theme"
+
+dump "dash-to-panel" "/org/gnome/shell/extensions/dash-to-panel/" "settings"
+dump "mouse" "/org/gnome/desktop/peripherals/mouse/" "settings"
+dump "touchpad" "/org/gnome/desktop/peripherals/touchpad/" "settings"
+dump "key-bind-0" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "settings"
+dump "power" "/org/gnome/settings-daemon/plugins/power/" "settings"
