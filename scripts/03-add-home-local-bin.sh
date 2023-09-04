7d57d906f356d45b17447e33df7eb0efb38cd756
Matt Proud <laidbackware.git@gmail.com>
2023-09-04 21:22:04 +0100

    again

--- scripts/03-add-home-local-bin.sh
+++ scripts/03-add-home-local-bin.sh
@@ -6,6 +6,6 @@ mkdir -p /home/$USER/.local/bin
 
 path_string="/home/${USER}/.local/bin"
 if ! sudo grep -Fxq "$path_string" /home/${USER}/.bashrc; then
-    echo "${PATH}:${path_string}" >> /home/${USER}/.bashrc
+    echo "export PATH=${PATH}:${path_string}" >> /home/${USER}/.bashrc
     echo "You must create a new shell to have $HOME/.local/bin added to your path"
 fi
